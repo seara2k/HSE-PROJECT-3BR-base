440c734b8bbfed3ad4705709641a5107696eed90
Sonanted <sonanted@gmail.com>
2020-06-25 22:38:49 +0300

    manuals changed

--- Work/Scripts/constants.py
+++ Work/Scripts/constants.py
@@ -1,4 +1,20 @@
-# Windows sizes
+"""
+Изменяемые константы приложения
+----------
+main_window - размер главного окна приложения
+base_stats_window - размер окна вывода базовой статистики
+summary_table_window - размер окна вывода сводной таблицы
+about_program_window - размер окна "о программе"
+add_element_window - размер окна добавления строки в таблицу
+bar_chart_variables_window - размер окна выбора элементов столбчатой диаграммы
+base_stats_variables_window - размер окна выбора элементов базовой статистики
+box_whiskers_variables_window - размер окна выбора элементов диаграммы Бокса-Вискера
+scatter_variables_window - размер окна выбора элементов диаграммы рассеяния
+histogram_variables_window - размер окна выбора элементов гистограммы
+summary_table_variables_window - размер окна выбора элементов сводной таблицы
+----------
+Автор: Никоненко А.Р.
+"""
 main_window = "1000x550"
 base_stats_window = "1000x350"
 summary_table_window = "1000x350"
